cd704a9fe01caef0979ef56bdbb1b597b6f08936
Maxcouturier <couturier.maxence@gmail.com>
2019-11-18 15:41:21 +0100

    Jusqu'à fonctio 4

--- script.js
+++ script.js
@@ -11,21 +11,38 @@ var footer = document.getElementsByTagName("footer");
 let i=0 ;
 footer[0].addEventListener('click', function(){
   i++;
-  console.log(`Clic numéro ${i}`);
+  console.log("Clic numéro " + i);
 });
 
 // Fonctionnalité 2 :
 
-document.getElementById("navbarHeader").addEventListener("click",function(){
-  document.getElementById("navbarHeader").classList.toggle('collapse');
+let navbar = document.getElementsByClassName("navbar-toggler")[0];
+navbar.addEventListener('click', function(){
+navbar.classList.toggle("collapse");
 });
-
-
+console.log(navbar.className)
 
 // Fonctionnalité 3 :
 
+let firstCard = document.getElementsByClassName("card mb-4 box-shadow")[0];
+var editButton = document.getElementsByClassName('btn btn-sm btn-outline-secondary')[0];
+editButton.addEventListener('click', function(){
+firstCard.style.color = 'red';
+});
+
 
 // Fonctionnalité 4 :
+
+let editButton2 = document.getElementsByClassName("btn-outline-secondary")[1];
+let secondCard = document.getElementsByClassName("card-text")[1];
+editButton2.addEventListener('click', function(){
+if (secondCard.style.color === 'green') { 
+  secondCard.style.color = '' ; 
+} else {
+  secondCard.style.color = 'green';
+}
+});
+
 
 
 // Fonctionnalité 5 :
